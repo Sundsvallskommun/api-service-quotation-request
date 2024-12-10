dc2fa8852cc1b6b55d3ba3e2193494dbecf1cdff
cheezi0747 <linus.sjolinder@b3.se>
2024-12-10 10:56:41 +0100

    UF-11274: Fix deperacted mockbean

--- src/test/java/se/sundsvall/quotationrequest/api/QuotationRequestResourceFailuresTest.java
+++ src/test/java/se/sundsvall/quotationrequest/api/QuotationRequestResourceFailuresTest.java
@@ -11,7 +11,7 @@ import java.util.Map;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.reactive.server.WebTestClient;
 import org.zalando.problem.Problem;
@@ -32,7 +32,7 @@ class QuotationRequestResourceFailuresTest {
 	@Autowired
 	private WebTestClient webTestClient;
 
-	@MockBean
+	@MockitoBean
 	private HelpdeskService helpdeskServiceMock;
 
 	@Test
